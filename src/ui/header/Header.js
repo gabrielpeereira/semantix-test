ca51c7b1e53b99a66fb5f36370dbde2c00eae9bf
Gabriel <gabriel.developer@outlook.com>
2020-04-05 15:17:38 -0300

    ajustando todo css

--- src/ui/header/Header.js
+++ src/ui/header/Header.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Header.css'
+import { Link } from 'react-router-dom';
 import { FiSearch, FiLogOut, FiFolder } from 'react-icons/fi'
 
 export default function Header() {
-    return(
+    return (
         <header className="menu">
-            <div class="menu-container">
-            <Link className="back-link" to="/">
-            <FiFolder size={16} color="#fff" />
-                Page 1
-                </Link>
 
-                <Link className="back-link" to="/page2">
-                <FiSearch size={16} color="#fff" />
-                    Page 2
-                </Link>
-                <FiLogOut size={16} color="#fff" >
-                    Page 2  </FiLogOut>        
+            <div className="menu-pages">
+                <div className="menu-page1">
+                    <Link className="back-link" to="/">
+                        <FiFolder size={16} color="#fff" />
+                            Page 1
+                    </Link>
+                </div>
+
+                <div className="menu-page2">
+                    <Link className="back-link" to="/page2">
+                        <FiSearch size={16} color="#fff" />
+                        Page 2
+                    </Link>
+                </div>
+
             </div>
-       
+
+            <div>
+                <FiLogOut size={16} color="#fff" >
+                    Logout  </FiLogOut>
+            </div>
+
+
         </header>
     );
 }
